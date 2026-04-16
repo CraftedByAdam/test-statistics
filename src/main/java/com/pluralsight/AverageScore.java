f4a6b94b3cc9703bc883bf7d466977f3927a3670
Your Name <youremail@example.com>
2026-04-16 15:31:38 -0400

    completed the average test score.

--- src/main/java/com/pluralsight/AverageScore.java
+++ src/main/java/com/pluralsight/AverageScore.java
@@ -1,6 +1,6 @@
 package com.pluralsight;
 
-public class MainTestStatistic {
+public class AverageScore {
     public static void main(String[] args) {
         int[] testScore = {67, 73, 89, 25, 98, 100, 92, 62, 71, 10};
 
@@ -8,8 +8,6 @@ public class MainTestStatistic {
 
         for (int score : testScore) {
             total += score;
-
-
         }
         System.out.println(total);
     }
