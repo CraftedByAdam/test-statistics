98261e92138ad0eeba147a25ebe09b39102420ec
Your Name <youremail@example.com>
2026-04-16 15:30:30 -0400

    Initial commit

---
+++ src/main/java/com/pluralsight/AverageScore.java
@@ -0,0 +1,16 @@
+package com.pluralsight;
+
+public class MainTestStatistic {
+    public static void main(String[] args) {
+        int[] testScore = {67, 73, 89, 25, 98, 100, 92, 62, 71, 10};
+
+        int total = 0;
+
+        for (int score : testScore) {
+            total += score;
+
+
+        }
+        System.out.println(total);
+    }
+}
